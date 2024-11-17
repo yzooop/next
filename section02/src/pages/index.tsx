399f55484327943a4bfe48c6e5d43625ddfe97e1
yzooop <qkrduswn0404@gmail.com>
2024-11-18 00:12:57 +0900

    💄 books UI

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,13 +1,25 @@
 import { ReactNode } from "react";
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
+import BookItem from "@/components/book-item";
+import books from "@/mock/books.json";
 
 export default function Home() {
     return (
-        <>
-            <h1 className={style.h1}>index</h1>
-            <h2 className={style.h2}>hello</h2>
-        </>
+        <div className={style.container}>
+            <section>
+                <h3>지금 추천하는 도서</h3>
+                {books.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </section>
+            <section>
+                <h3>등록된 모든 도서</h3>
+                {books.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </section>
+        </div>
     );
 }
 
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,12 +1,16 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-    const router = useRouter();
-    const { q } = router.query;
-
-    return <h1>search {q}</h1>;
+    return (
+        <div>
+            {books.map((book) => (
+                <BookItem key={book.id} {...book} />
+            ))}
+        </div>
+    );
 }
 
 Page.getLayout = (page: ReactNode) => {
